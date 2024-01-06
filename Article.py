1a3c46e7a06904757833539707b2d5ed4843fd89
Pawel <ptomasiewicz.budownictwo@gmail.com>
2024-01-06 14:58:57 +0100

    refactor Article and NewsAPI to safer approach

--- Article.py
+++ Article.py
@@ -1,13 +1,29 @@
+import logging
 class Article:
     def __init__(self,article_data):
-        self.source=article_data['source']['name']
-        self.author=article_data['author']
-        self.title=article_data['title']
-        self.description=article_data['description']
-        self.url=article_data['url']
-        self.urlToImage=article_data['urlToImage']
-        self.publishedAt=article_data['publishedAt']
-        self.content=article_data['content']
+        self.article_data=article_data
+        self.source=None
+        self.author=None
+        self.title=None
+        self.description=None
+        self.url=None
+        self.urlToImage=None
+        self.publishedAt=None
+        self.content=None
+        try:
+            self.parse_data()
+        except Exception as e:
+            logging.error(f'Error while parsing data taken from NewsAPI client: {e}')
+
+    def parse_data(self):
+        self.source=self.article_data.get('source', {}).get('name')
+        self.author= self.article_data.get('author', 'Unknown')
+        self.title=self.article_data.get('title', 'Unknown')
+        self.description=self.article_data.get('description', 'No description')
+        self.url=self.article_data.get('url', 'No url')
+        self.urlToImage=self.article_data.get('urlToImage', 'No image')
+        self.publishedAt=self.article_data.get('publishedAt', 'No image')
+        self.content=self.article_data.get('content', 'No content avalable')
 
     def get_title(self):
         return self.title
--- NewsAPI.py
+++ NewsAPI.py
@@ -1,15 +1,29 @@
+import logging
 from newsapi import NewsApiClient
 from Article import Article
 from variables import API_KEY
 
 class NewsAPI:
     def __init__(self,category):
-        self.category =  category
-        self.client = NewsApiClient(api_key = API_KEY)
-        self.download_articles()
+        self.category = category
+        self.client = None
+        self.articles = []
+        self.initialize_client()
+
+    def initialize_client(self):
+        try:
+            self.client = NewsApiClient(api_key=API_KEY)
+            self.download_articles()
+        except Exception as e:
+            logging.error(f'Error while connecting with NewsAPI client: {e}')
 
     def download_articles(self):
-        self.articles=[Article(input) for input in self.client.get_everything(q=self.category,sort_by='popularity', language='en')['articles']]
+        if self.client:
+            try:
+                response = self.client.get_everything(q=self.category, sort_by='popularity', language='en')
+                self.articles = [Article(article_data) for article_data in response['articles']]
+            except Exception as e:
+                logging.error(f'Error while downloading articles from NewsAPI: {e}')
 
     def get_titles_of_articles(self):
         return [article.get_title() for article in self.articles]
